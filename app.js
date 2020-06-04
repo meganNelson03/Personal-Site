c0bb20c0640db879400b8d936741cd73601f3cdf
Megan Nelson <menelson@vassar.edu>
2020-06-04 22:33:32 +1200

    add contact form, cv section

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/art", (req, res) => {
+  res.send("art site");
+});
+
+
+app.post("/", (req, res) => {
+  // send email on form POST request
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+
+  res.send(`Your name is ${name}, your email is ${email}, your message is ${message}`);
+})
 
 app.listen(8082, () => {
   console.log("Listening on port 8082");
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,15 @@
+$(document).scroll(function() {
+
+  $(".navbar").addClass("scrolled");
+
+  if ($(document).scrollTop() < 200) {
+    $(".navbar").removeClass("scrolled");
+    $(".navbar").addClass("unscrolled");
+  }
+
+});
+
+
 $(".btn").mouseover(function() {
   $(".fas").addClass("animated");
 });
@@ -5,3 +17,7 @@ $(".btn").mouseover(function() {
 $(".btn").mouseout(function () {
   $(".fas").removeClass("animated");
 });
+
+$(".contact-section p").hover(function() {
+  $(".contact-section p").addClass("contactAnimation");
+})
