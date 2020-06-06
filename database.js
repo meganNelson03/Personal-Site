5484937d01bd765021c00b45719bbf3e41039fd7
Megan Nelson <menelson@vassar.edu>
2020-06-07 11:29:34 +1200

    art page footer and header section, nav

--- database.js
+++ database.js
@@ -1,124 +1,142 @@
 var pieces = [
 	{
 		name: "The Beach",
-		image: "/imgs/the_beach.png",
+		image: "/the_beach.png",
+		description: "black and white"
+	},
+	{
+		name: "Flower",
+		image: "/flower.jpeg",
+		description: "pen and ink"
+
+	},
+	{
+		name: "something",
+		image: "/something.png",
 		description: "black and white"
 
 	},
 	{
-		name: "Flower",
-		image: "/imgs/flower.jpeg",
-		description: "pen and ink"
-	},
-	{
-		name: "something",
-		image: "/imgs/something.png",
+		name: "indoors",
+		image: "/indoors.jpg",
 		description: "black and white"
-	},
-	{
-		name: "Other other",
-		image: "/imgs/joyce3.jpg",
-		description: "marker"
-	},
-	{
-		name: "house",
-		image: "/imgs/house.jpg",
-		description: "marker"
-	},
-	{
-		name: "grandma",
-		image: "/imgs/grandma.jpg",
-		description: "marker"
-	},
-	{
-		name: "Other Joyce",
-		image: "/imgs/joyce2.jpg",
-		description: "colored pencil"
-	},
-	{
-		name: "Joyce",
-		image: "/imgs/joyce.jpg",
-		description: "colored pencil"
-	},
-	{
-		name: "Ali Reza",
-		image: "/imgs/alireza.jpg",
-		description: "colored pencil"
+
 	},
 	{
 		name: "chairs",
-		image: "/imgs/chair.jpg",
+		image: "/chair.jpg",
 		description: "pen and ink"
+
+	},
+	{
+		name: "Other other",
+		image: "/joyce3.jpg",
+		description: "marker"
+
+	},
+	{
+		name: "house",
+		image: "/house.jpg",
+		description: "marker"
+
+	},
+	{
+		name: "grandma",
+		image: "/grandma.jpg",
+		description: "marker"
+
+	},
+	{
+		name: "Other Joyce",
+		image: "/joyce2.jpg",
+		description: "colored pencil"
+
+	},
+	{
+		name: "Joyce",
+		image: "/joyce.jpg",
+		description: "colored pencil"
+
+	},
+	{
+		name: "Ali Reza",
+		image: "/alireza.jpg",
+		description: "colored pencil"
+
 	},
 	{
 		name: "Tents",
-		image: "/imgs/camping.jpg",
+		image: "/camping.jpg",
 		description: "marker"
+
 	},
-	{
-		name: "amanda",
-		image: "/imgs/amanda.jpg",
-		description: "colored pencil"
-	},
+
 	{
 		name: "myself",
-		image: "/imgs/myself.jpg",
+		image: "/myself.jpg",
 		description: "oil painting"
+
 	},
 	{
 		name: "sugarhouse coffee",
-		image: "/imgs/sugarhouse.jpg",
+		image: "/sugarhouse.jpg",
 		description: "watercolor"
+
 	},
 	{
 		name: "outdoors",
-		image: "/imgs/outdoors.jpg",
+		image: "/outdoors.jpg",
 		description: "colored pencil"
+
 	},
 	{
 		name: "outdoors",
-		image: "/imgs/coffee.png",
+		image: "/coffee.png",
 		description: "coffee painting"
+
 	},
 	{
 		name: "little statue",
-		image: "/imgs/littlestatue.jpg",
+		image: "/littlestatue.jpg",
 		description: "coffee painting"
+
 	},
 	{
 		name: "inside",
-		image: "/imgs/coffee.jpg",
+		image: "/coffee.jpg",
 		description: "coffee paining"
+
 	},
 	{
 		name: "cagefan.jpg",
-		image: "/imgs/cagefan.jpg",
+		image: "/cagefan.jpg",
 		description: "black and white"
+
 	},
-	{
-		name: "indoors",
-		image: "/imgs/indoors.jpg",
-		description: "black and white"
-	},
+
 	{
 		name: "purple girl",
-		image: "/imgs/purple.jpeg",
+		image: "/purple.jpeg",
 		description: "digital"
+
 	},
 	{
 		name: "nscon logo",
-		image: "/imgs/nsconlogo.png",
+		image: "/nsconlogo.png",
 		description: "black and white"
+
+	},
+	{
+		name: "amanda",
+		image: "/amanda.jpg",
+		description: "colored pencil"
+
 	},
 	{
 		name: "stuff",
-		image: "/imgs/stuff.jpg",
+		image: "/stuff.jpg",
 		description: "marker"
-	},
-	{
-		name: "crossword puzzle",
-		image: "/imgs/crossword.jpg",
-		description: "pen"
+
 	}
 ]
 
---
+++ public/scripts/art.js
@@ -0,0 +1,15 @@
+
+
+var $grid = $('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  resize: true,
+  // columnWidth: 30,
+  percentPosition: true
+});
+
+$grid.imagesLoaded().progress(
+  function() {
+    $grid.masonry("layout");
+  }
+)
